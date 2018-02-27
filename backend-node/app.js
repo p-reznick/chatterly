d89debb31b67a80e53b863585ff78295eabf8075
Peter Reznick <preznick@gmail.com>
2018-02-26 22:27:32 -0500

    add create user API endpoint

--- backend-node/app.js
+++ backend-node/app.js
@@ -10,31 +10,45 @@ const db = mysql.createPool({
 });
 
 // login user GET users/:id
-// send message POST messages
-// fetch messages GET messages
-// fetch new messages
-
-app.get('/test', function (req, res) {
-    db.getConnection(function (err, connection) {
-        if (err) {
-            res.status(501).send(err.message);
-            return;
-        }
-        connection.query('SELECT * FROM rooms', function (err, results, fields) {
-            if (err) {
-                res.status(501).send(err.message);
-                connection.release();
-                return;
-            }
-
-            res.json({
-                result: results,
-                backend: 'nodejs',
-            });
-            connection.release();
-        });
+// create user POST users/:handle
+app.post('/users/:handle', function (req, res) {
+  db.getConnection(function (err, connection) {
+    if (err) {
+        res.status(501).send(err.message);
+        return;
+    }
+    connection.query('INSERT INTO users (handle) VALUES (?)', [req.params['handle']], function (err, results, field) {
+      if (err) {
+          res.status(501).send(err.message);
+          connection.release();
+          return;
+      }
+      res.status(201);
+      res.send(req.params['handle'] + ' delivered!');
     });
+  });
 });
+
+// app.get('/test', function (req, res) {
+//     db.getConnection(function (err, connection) {
+//         if (err) {
+//             res.status(501).send(err.message);
+//             return;
+//         }
+//         connection.query('SELECT * FROM rooms', function (err, results, fields) {
+//             if (err) {
+//                 res.status(501).send(err.message);
+//                 connection.release();
+//                 return;
+//             }
+//             res.json({
+//                 result: results,
+//                 backend: 'nodejs',
+//             });
+//             connection.release();
+//         });
+//     });
+// });
 
 app.listen(8000, function() {
     console.log('Listening on port 8000');
