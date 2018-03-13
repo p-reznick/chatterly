c9f2897f0228f12da1e2ea07fae523c3ee3928b2
Peter Reznick <preznick@gmail.com>
2018-03-12 21:50:43 -0400

    add timestamps to users, rooms, comments

--- db/sql/init.sql
+++ db/sql/init.sql
@@ -2,19 +2,22 @@ USE challenge;
 
 CREATE TABLE rooms (
   id serial primary key,
-  name varchar(25) NOT NULL
+  name varchar(25) NOT NULL,
+  created_at datetime default CURRENT_TIMESTAMP
 );
 
 CREATE TABLE users (
   id serial primary key,
-  handle varchar(25) NOT NULL UNIQUE
+  handle varchar(25) NOT NULL UNIQUE,
+  created_at datetime default CURRENT_TIMESTAMP
 );
 
 CREATE TABLE comments (
   id serial primary key,
   user_id integer NOT NULL REFERENCES users(id),
   body varchar(800),
-  room_id integer NOT NULL REFERENCES rooms(id)
+  room_id integer NOT NULL REFERENCES rooms(id),
+  created_at datetime default CURRENT_TIMESTAMP
 );
 
 INSERT INTO rooms (name)
